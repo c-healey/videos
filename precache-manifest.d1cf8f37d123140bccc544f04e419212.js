6b3c1677f259315a204ef14f562bb7dbfa470e07
Cathy <cathy.healey@gmail.com>
2020-03-06 15:15:32 -0500

    Updates

---
+++ precache-manifest.d1cf8f37d123140bccc544f04e419212.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "502c6e9a709fc09bd09287e6729f91fc",
+    "url": "/videos/index.html"
+  },
+  {
+    "revision": "e61b52c091ac334d0325",
+    "url": "/videos/static/css/main.8ad48a67.chunk.css"
+  },
+  {
+    "revision": "97cbe88f29d74121f4fb",
+    "url": "/videos/static/js/2.1902ad20.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/videos/static/js/2.1902ad20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e61b52c091ac334d0325",
+    "url": "/videos/static/js/main.667330bc.chunk.js"
+  },
+  {
+    "revision": "e9ef7060a80fc9cfd8d4",
+    "url": "/videos/static/js/runtime-main.bef75cc8.js"
+  }
+]);
